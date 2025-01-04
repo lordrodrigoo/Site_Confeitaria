7ca5ee4927d891abf3ed19a7eee0a2701ac82de9
rodrigo <rodrigog3wconcept@gmail.com>
2025-01-03 22:50:00 -0300

    feat: create pages: produtos / bolos decorados / sobre

--- project/urls.py
+++ project/urls.py
@@ -22,4 +22,17 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('produtos/', views.produtos, name='produtos'),
+    path('bolos-decorados/', views.bolos_decorados, name='bolos_decorados'),
+    path('sobre/', views.sobre, name='sobre'),
+
+
+
+    path('produtos/bolos-com-cobertura/', views.bolos_com_cobertura, name='produtos/bolos_com_cobertura'),
+    path('produtos/bolos-da-vovo/', views.bolos_da_vovo, name='produtos/bolos_da_vovo'),
+    path('produtos/bolos-de-pote/', views.bolos_de_pote, name='produtos/bolos_de_pote'),
+    path('produtos/cones/', views.cones, name='produtos/cones'),
+    path('produtos/copos_felicidade/', views.cones, name='produtos/copos_felicidade'),
+    path('produtos/doces_sobremesas/', views.doces_sobremesas, name='produtos/doces_sobremesas'),
+    path('produtos/sobremesas_familia/', views.sobremesas_familia, name='produtos/sobremesas_familia'),
+
 ]
--- store/views.py
+++ store/views.py
@@ -7,3 +7,30 @@ def home(request):
 
 def produtos(request):
     return render(request, 'store/produtos.html')
+
+def bolos_com_cobertura(request):
+    return render(request, 'store/bolos_com_cobertura.html')
+
+def bolos_da_vovo(resquest):
+    return render(resquest, 'produtos/bolos_da_vovo.html')
+
+def bolos_de_pote(request):
+    return render(request, 'produtos/bolos_de_pote.html')
+
+def cones(request):
+    return render(request, 'produtos/cones.html')
+
+def copos_felicidade(request):
+    return render(request, 'produtos/copos_felicidade.html')
+
+def doces_sobremesas(request):
+    return render(request, 'produtos/doces_sobremesas.html')
+
+def sobremesas_familia(request):
+    return render(request, 'produtos/sobremesas_familia.html')
+
+def bolos_decorados(request):
+    return render(request, 'store/bolos_decorados.html')
+
+def sobre(request):
+    return render(request, 'store/sobre.html')
